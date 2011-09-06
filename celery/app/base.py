882fa07f1ec78fcd199104cd894589a8fe077275
Ask Solem <ask@celeryproject.org>
2011-09-06 12:16:10 +0100

    Moves celery.task.control -> celery.app.control

--- celery/app/base.py
+++ celery/app/base.py
@@ -88,7 +88,7 @@ class BaseApp(object):
     events_cls = "celery.events:Events"
     loader_cls = "celery.loaders.app:AppLoader"
     log_cls = "celery.log:Logging"
-    control_cls = "celery.task.control:Control"
+    control_cls = "celery.app.control:Control"
 
     _pool = None
 
@@ -360,7 +360,7 @@ class BaseApp(object):
     @cached_property
     def control(self):
         """Controlling worker nodes.  See
-        :class:`~celery.task.control.Control`."""
+        :class:`~celery.app.control.Control`."""
         return instantiate(self.control_cls, app=self)
 
     @cached_property
